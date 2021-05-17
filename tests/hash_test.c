2217db0ef6035b4ec541ff7dd664dda337dc13a8
MinigalievNikita <70330429+MinigalievNikita@users.noreply.github.com>
2021-05-17 14:36:38 +0300

    Create hash_test.c

---
+++ tests/hash_test.c
@@ -0,0 +1,49 @@
+#include "LRU2_hash.h"
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+struct node {
+    struct node * parent;
+    struct node * left;
+    struct node * right;
+    int color;
+    struct buffer * address;
+    int hist1; 
+    int value; 
+    int last;  
+};
+
+
+void rand_test();
+
+int main() {
+    srand(time(NULL));
+    
+    rand_test();
+    return 0;
+}
+
+void rand_test() {
+    int size = abs(rand()%1000 + 1);
+    int sizearr = (abs(rand()%100 + 1)) * size;
+    int *arr = calloc(sizearr, sizeof(int));
+    struct HashTab *hashtab = create_hashtab(size);
+    struct node *value = (struct node*) calloc (1, sizeof (struct node));
+    for (int i = 0; i < sizearr; ++i) {
+         add_value(hashtab, arr[i], value);
+    }
+    for (int i = 0; i < sizearr; ++i) {
+        assert(get_value(hashtab, arr[i]) != NULL);
+    }
+    for (int i = 0; i < sizearr; ++i) {
+        del_value(hashtab, arr[i]);
+    }
+    for (int i = 0; i < sizearr; ++i) {
+        assert(get_value(hashtab, arr[i]) == NULL);
+    }
+    free_hashtab(hashtab);
+    printf("Test complete");
+}
