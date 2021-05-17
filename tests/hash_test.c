8e28bd66a02afd357e12101de776da439884c0fd
MinigalievNikita <70330429+MinigalievNikita@users.noreply.github.com>
2021-05-17 15:02:25 +0300

    Update hash_test.c

--- tests/hash_test.c
+++ tests/hash_test.c
@@ -30,8 +30,10 @@ void rand_test() {
     int size = abs(rand()%1000 + 1);
     int sizearr = (abs(rand()%100 + 1)) * size;
     int *arr = calloc(sizearr, sizeof(int));
+    assert(arr);
     struct HashTab *hashtab = create_hashtab(size);
     struct node *value = (struct node*) calloc (1, sizeof (struct node));
+    assert(value);
     printf("Size of hashtab: %d Pages: %d\n",size, sizearr);
     for (int i = 0; i < sizearr; ++i) {
          add_value(hashtab, arr[i], value);
